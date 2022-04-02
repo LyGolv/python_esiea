4a2eeda3554d47f642dadddc0184c017c5f60e97
Guimdo Lioguy <vianel.lioguy@gmail.com>
2022-04-02 19:39:48 +0200

    ajout de la commande ban, unban, banlsId

--- td4_python/modules/helpcommand/cog.py
+++ td4_python/modules/helpcommand/cog.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 import logging
-
-from pydantic import EnumMemberError
+from datetime import datetime
 
 class HelpCommand(commands.Cog, name="helpcommand"):
     """Show the help command to user"""
@@ -17,12 +16,17 @@ class HelpCommand(commands.Cog, name="helpcommand"):
         embed : discord.Embed = discord.Embed(
             title = "# Help Commands",
             description = 'List of all available commands',
-            colour = discord.Colour.blue()     
+            colour = discord.Colour.blue(),
+            timestamp=datetime.utcnow()
         )
         embed.set_thumbnail(url=self.bot.user.avatar_url)
-        embed.add_field(name= "!hello", value='say hello', inline=True)
-        embed.add_field(name= "!help", value='ask help', inline=True)
-        embed.set_footer(text="Don't forget to add a reaction if this message help u :)")
+        embed.add_field(name= "!hello", value='say hello', inline=False)
+        embed.add_field(name= "!help", value='ask help', inline=False)
+        embed.add_field(name= "!clear NUMBER", value='deletes the specified number of messages ', inline=False)
+        embed.add_field(name= "!ban @USER REASON", value='Use to ban user of channel', inline=False)
+        embed.add_field(name= "!unban UserID REASON", value='Use to unban user', inline=False)
+        embed.add_field(name= "!banlsId", value='List all of banned users', inline=False)
+        embed.set_footer(text="U_U is useful?? :)")
         logging.warning("[!Help message send for user]")
         await ctx.send(embed=embed)
 
---
+++ td4_python/modules/manage/cog.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+import logging
+
+from zmq import Message
+
+class ManageCommand(commands.Cog, name="helpcommand"):
+    """Command to manage channel"""
+    
+    def __init__(self, bot : commands.Bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def clear(self, ctx: commands.Context, nombre: int):
+        """Clear message on channel"""
+        messages = await ctx.channel.history(limit = nombre + 1).flatten()
+        for message in messages:
+            await message.delete()
+    
+    @commands.command()
+    async def ban(self, ctx: commands.Context, user : discord.User, *reason):
+        """use to ban user"""
+        reason = " ".join(reason)
+        await ctx.guild.ban(user, reason=reason)
+        await ctx.send(f"{user} à été ban pour la raison suivante : {reason}.")
+
+    @commands.command()
+    async def unban(self, ctx: commands.Context, id, *reason):
+        """Use to unban user"""
+        reason = " ".join(reason)
+        banned_users = await ctx.guild.bans()
+        for i in banned_users:
+            if str(i.user.id) == str(id):
+                await ctx.guild.unban(i.user, reason=reason)
+                await ctx.send(f"{i.user.mention} à été unban")
+                return
+        await ctx.send(f"{id} n'est pas dans la liste des bannis")
+        
+    @commands.command()
+    async def banlsId(self, ctx: commands.Context):
+        """list all ban users"""
+        bans = "La liste des utilisateurs banni du serveur sont: \n"
+        banned_users = await ctx.guild.bans()
+        for i in banned_users:
+            bans += f"--> {i.user.name} : {i.user.id}\n"
+        await ctx.send(bans)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(ManageCommand(bot))
